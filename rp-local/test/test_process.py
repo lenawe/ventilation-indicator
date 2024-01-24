8cf2fd9dc82d44a0a8e1fe0cc4856146d37640aa
lenawe <50956240+lenawe@users.noreply.github.com>
2024-01-24 22:11:32 +0100

    VI-17 Add test case 1 (invalid input)

--- rp-local/test/test_process.py
+++ rp-local/test/test_process.py
@@ -9,5 +9,13 @@ class TestProcess(unittest.TestCase):
         temperature = 25.45636457
         expected_result = '{"humidity": 50.5636784, "temperature": 25.45636457}'
         self.assertEqual(process.get_payload_json(humidity, temperature), expected_result)
+
+    def test_get_payload_json_invalid_input(self):
+        # Test case 2: Invalid input
+        humidity = "high"
+        temperature = 25.45636457
+        with pytest.raises(ValueError):
+            process.get_payload_json(humidity, temperature)
+
 if __name__ == '__main__':
     unittest.main()
