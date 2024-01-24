e94f59d7acfa5f503e43bb8e12d9c88616cfa240
lenawe <50956240+lenawe@users.noreply.github.com>
2024-01-24 08:43:24 +0100

    VI-15 Add exception handling

--- rp-local/bme280/read.py
+++ rp-local/bme280/read.py
@@ -2,9 +2,13 @@ import smbus2
 import bme280
 
 def load_parameter(port, address):
-    bus = smbus2.SMBus(port)
-    bme280.load_calibration_params(bus, address)
-    return bus
+    try:
+        bus = smbus2.SMBus(port)
+        bme280.load_calibration_params(bus, address)
+        return bus
+    except Exception as e:
+        print(f"Error loading parameters: {e}")
+        return None
 
 def read_data(bus, address):
     sensor_data = bme280.sample(bus, address)
