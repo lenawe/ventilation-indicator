e63db9e8395f2597cd09dcdedd012591420b60d9
lenawe <50956240+lenawe@users.noreply.github.com>
2024-01-26 22:40:40 +0100

    VI-16 Test reading data

--- rp_local/test/test_read.py
+++ rp_local/test/test_read.py
@@ -19,3 +19,18 @@ class TestRead(unittest.TestCase):
         self.assertEqual(result, mock_bus)
         mock_SMBus.assert_called_once_with(port)
         mock_load_calibration_params.assert_called_once_with(mock_bus, address)
+
+    def test_read_data(self):
+
+        mock_sensor_data = MagicMock()
+        mock_sensor_data.humidity = 50.0
+        mock_sensor_data.temperature = 25.0
+
+        mock_bus = MagicMock()
+        address = 0x76
+
+        with patch('rp_local.sensor.read.bme280.sample', return_value=mock_sensor_data):
+            humidity, temperature = read_data(mock_bus, address)
+
+        self.assertEqual(humidity, 50.0)
+        self.assertEqual(temperature, 25.0)
