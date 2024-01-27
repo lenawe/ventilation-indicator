7fb1cf2648329ef12bfa3b97cbadeea7d9a82ded
lenawe <50956240+lenawe@users.noreply.github.com>
2024-01-27 20:46:13 +0100

    VI-21 Add IoT policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,37 @@ provider "aws" {
 resource "aws_iot_thing" "this" {
     name = "thing_${var.thing_name}"
 }
+
+resource "aws_iot_policy" "this" {
+    name = "policy_${var.thing_name}"
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Effect": "Allow",
+              "Action": "iot:Connect",
+              "Resource": "*"
+          },
+          {
+              "Effect": "Allow",
+              "Action": "iot:Receive",
+              "Resource": "*"
+          },
+          {
+              "Effect": "Allow",
+              "Action": "iot:Publish",
+              "Resource": "*"
+          },
+          {
+              "Effect": "Allow",
+              "Action": "iot:Subscribe",
+              "Resource": "*"
+          },
+          {
+              "Effect": "Allow",
+              "Action": "iot:PublishRetain",
+              "Resource": "*"
+          }
+      ]
+    })
+}
