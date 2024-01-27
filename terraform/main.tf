18ec065a2aa5497d0553fc4a72d4a3bab2705c43
lenawe <50956240+lenawe@users.noreply.github.com>
2024-01-27 20:49:15 +0100

    VI-21 Attach policy to thing

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,3 +49,8 @@ resource "aws_iot_thing_principal_attachment" "this" {
     thing = aws_iot_thing.this.name
     principal = aws_iot_certificate.this.arn
 }
+
+resource "aws_iot_policy_attachment" "this" {
+    policy = aws_iot_policy.this.name
+    target = aws_iot_certificate.this.arn
+}
