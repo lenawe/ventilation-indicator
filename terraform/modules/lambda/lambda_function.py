77e218720ec99d992d99e09eff87ae0b7a0f5463
lenawe <50956240+lenawe@users.noreply.github.com>
2024-01-28 14:05:22 +0100

    VI-24 Prepare mail

--- terraform/modules/lambda/lambda_function.py
+++ terraform/modules/lambda/lambda_function.py
@@ -5,3 +5,25 @@ client = boto3.client('ses', region_name='eu-north-1')
 def lambda_handler(event, context):
     message_text = "This is a mail for testing.",
     subject = "Please open the windows!"
+    recipients = [] # enter destination address
+    sender = 'source@mail.com' # enter source address
+    
+    response = client.send_email(
+        Destination={
+            'ToAddresses': recipients
+        },
+            
+        Message={
+            'Body': {
+                'Text': {
+                    'Charset': 'UTF-8',
+                    'Data': ''.join(message_text)
+                }
+            },
+            'Subject': {
+                'Charset': 'UTF-8',
+                'Data': subject,
+            },
+        },
+        Source = sender
+    )
