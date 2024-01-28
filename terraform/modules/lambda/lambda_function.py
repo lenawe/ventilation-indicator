1df35bddcd9ef6eb646b38fefdf3dfeb6f00c2a0
lenawe <50956240+lenawe@users.noreply.github.com>
2024-01-28 12:22:34 +0100

    VI-23 Init lambda_function.py

--- terraform/modules/lambda/lambda_function.py
+++ terraform/modules/lambda/lambda_function.py
@@ -1,1 +1,6 @@
-print('Hello from lambda_function.py')
+import boto3
+
+client = boto3.client('ses', region_name='eu-north-1')
+
+def lambda_handler(event, context):
+    print('Hello from lambda_function.py')
