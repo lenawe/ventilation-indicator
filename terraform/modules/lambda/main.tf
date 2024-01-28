4261549318d2edbb92669e7975efd26ce3055cec
lenawe <50956240+lenawe@users.noreply.github.com>
2024-01-28 12:08:32 +0100

    VI-23 Add role and apply policy

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -47,3 +47,9 @@ resource "aws_iam_policy" "this" {
         ]
     })
 }
+
+resource "aws_iam_role" "this" {
+    name               = "role_notification${var.thing_name}"
+    assume_role_policy = data.aws_iam_policy_document.this.json
+    managed_policy_arns = [ aws_iam_policy.this.arn ]
+}
