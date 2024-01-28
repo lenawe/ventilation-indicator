ce7690996eb1b938efe3f7352e801fb379ccaab3
lenawe <50956240+lenawe@users.noreply.github.com>
2024-01-28 14:03:08 +0100

    VI-24 Add subject to mail

--- terraform/modules/lambda/lambda_function.py
+++ terraform/modules/lambda/lambda_function.py
@@ -4,3 +4,4 @@ client = boto3.client('ses', region_name='eu-north-1')
 
 def lambda_handler(event, context):
     message_text = "This is a mail for testing.",
+    subject = "Please open the windows!"
