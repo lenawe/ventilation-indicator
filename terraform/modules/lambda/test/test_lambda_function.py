ca3f99e4c406ea5eaf3444af75ec8ddeae64612e
lenawe <50956240+lenawe@users.noreply.github.com>
2024-01-30 09:20:07 +0100

    VI-25 Unit test for weather API

--- terraform/modules/lambda/test/test_lambda_function.py
+++ terraform/modules/lambda/test/test_lambda_function.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import MagicMock, patch
-from ..lambda_function import lambda_handler, send_notification
+from ..lambda_function import lambda_handler, send_notification, get_outdoor_measurements
 
 class TestLambdaFunction(unittest.TestCase):
 
@@ -26,6 +26,32 @@ class TestLambdaFunction(unittest.TestCase):
         self.assertEqual(response['statusCode'], 200)
         self.assertTrue('Email Sent Successfully' in response['body'])
         self.assertTrue(test_message_id in response['body'])
+
+    @patch('urllib3.PoolManager')
+    def test_get_outdoor_measurements(self, pool_manager_mock):
+        # Test data
+        latitude = "12.345"
+        longitude = "67.890"
+        app_id = "test_app_id"
+        api_response_data = '''
+        {
+            "current": {
+                "humidity": 50,
+                "temp": 25
+            }
+        }
+        '''
+
+        # Mock HTTP response
+        http_response_mock = MagicMock()
+        http_response_mock.data.decode.return_value = api_response_data
+        pool_manager_mock.return_value.request.return_value = http_response_mock
+
+        # Call function
+        result = get_outdoor_measurements(latitude, longitude, app_id)
+
+        # Assertion
+        self.assertEqual(result, (25, 50))
 
 if __name__ == '__main__':
     unittest.main()
